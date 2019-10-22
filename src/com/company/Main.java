4850a36b2b3d22778804ade0b1089788e42de42d
stefanmaniga1 <stefanmaniga@gmail.com>
2019-10-22 13:28:38 +0300

    scuze sefu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("I own you, bitch");
-        System.out.println("Still owning your project. HAAAA");
+        System.out.println("Mirel boss 2019");
+        System.out.println("Sefu la ifon");
     }
 }
