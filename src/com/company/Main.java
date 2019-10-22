21ba2aa9835167a84fe0e2fb03e224fab86d6157
stefanmaniga1 <stefanmaniga@gmail.com>
2019-10-22 13:17:10 +0300

    owning mirel's project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Branch 1");
+        System.out.println("I own you, bitch");
+        System.out.println("Still owning your project. HAAAA");
     }
 }
